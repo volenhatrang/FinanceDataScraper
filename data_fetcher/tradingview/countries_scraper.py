ed79ac7cb4f081a1e5e5d5df06666ac76856c3ca
nhatrang <trangvo.beqholdings@gmail.com>
2025-03-03 16:18:12 +0700

    update scripts

--- data_fetcher/tradingview/countries_scraper.py
+++ data_fetcher/tradingview/countries_scraper.py
@@ -1,3 +1,4 @@
+# FINANCEDATASCRAPER/data_fetcher/tradingview/countries_scraper.py
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -5,44 +6,49 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import json
+import os
 
-def crawler_countries():
 
+def countries_scraper():
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
     # options.add_argument("--disable-gpu")
-    options.add_argument("--headless")  
-    # options.add_argument("--no-sandbox") 
-    # options.add_argument("--disable-dev-shm-usage")  
-    # options.add_argument("--window-size=1920,1080")  
-    # options.add_argument("--disable-software-rasterizer")
+    # options.add_argument("--no-sandbox")
     # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--disable-software-rasterizer")
 
-    # Remote WebDriver
+
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+
     URL = "https://www.tradingview.com/data-coverage/"
-    driver.get(URL) 
+    driver.get(URL)
 
     button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "Selectcountry")))
     button.click()
 
-    dialog = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-name="country-select-dialog"]')))
-    # print(dialog.get_attribute("outerHTML"))
+    dialog = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-name="country-select-dialog"]'))
+    )
 
     regions = dialog.find_elements(By.CLASS_NAME, "groupTitle-TiHRzx3B")
-
     data = []
 
     for region in regions:
         region_name = region.text.strip()
-        countries_list = [] 
-        
+        countries_list = []
+
         try:
             container = WebDriverWait(region, 5).until(
-                EC.presence_of_element_located((By.XPATH, "./following-sibling::div[contains(@class, 'marketItemsContainer-TiHRzx3B')]"))
+                EC.presence_of_element_located(
+                    (By.XPATH, "./following-sibling::div[contains(@class, 'marketItemsContainer-TiHRzx3B')]")
+                )
             )
 
-            countries = container.find_elements(By.XPATH, ".//div[contains(@class, 'iconColor-XLXs8O7w wrapTablet-XLXs8O7w')]")
+            countries = container.find_elements(
+                By.XPATH, ".//div[contains(@class, 'iconColor-XLXs8O7w wrapTablet-XLXs8O7w')]"
+            )
 
             for country in countries:
                 country_name = country.find_element(By.CLASS_NAME, "title-XLXs8O7w").text.strip()
@@ -56,7 +62,7 @@ def crawler_countries():
                 })
 
         except Exception as e:
-            print(f"No Country {region_name}: {e}")
+            print(f"No Country in {region_name}: {e}")
 
         if countries_list:
             data.append({
@@ -64,10 +70,16 @@ def crawler_countries():
                 "countries": countries_list
             })
 
-    json_output = json.dumps(data, indent=4, ensure_ascii=False)
+    tradingview_path = "/opt/airflow/FINANCEDATASCRAPER/database/scraping_raw_json/tradingview"
+    os.makedirs(tradingview_path, exist_ok=True)
 
-    # Save json file
-    tradingview_path = "./database/scraping_raw_json/tradingview"
+    json_output = json.dumps(data, indent=4, ensure_ascii=False)
     with open(f"{tradingview_path}/countries_with_flags.json", "w", encoding="utf-8") as f:
         f.write(json_output)
+
     driver.quit()
+    return "Countries scraped successfully!"
+
+
+if __name__ == "__main__":
+    countries_scraper()
--- data_fetcher/tradingview/exchanges_scraper.py
+++ data_fetcher/tradingview/exchanges_scraper.py
@@ -4,93 +4,96 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
 import json
 import time
+import os
 
 
-options = webdriver.ChromeOptions()
-# options.add_argument("--disable-gpu")
-options.add_argument("--headless")  
-# options.add_argument("--no-sandbox") 
-# options.add_argument("--disable-dev-shm-usage")  
-options.add_argument("--window-size=1920,1080")  
-# options.add_argument("--disable-software-rasterizer")
-# options.add_argument("--disable-dev-shm-usage")
+def exchanges_scraper():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")
 
-# Remote WebDriver
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
-URL = "https://www.tradingview.com/data-coverage/"
-driver.get(URL) 
-tradingview_path = "./database/scraping_raw_json/tradingview"
 
-# tabs = ["Popular", "Stocks& Indices", "Futures", "Forex", "Crypto", "Economy"]
-tabs = ["Popular", "Stocks& Indices", "Futures", "Forex", "Crypto"]
-exchange_data = []
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
 
-for tab_id in tabs:
-    # Start
-    tab_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, f"//button[@id='{tab_id}']"))
-    )
-    # tab_button.click()
-    driver.execute_script('arguments[0].click()', tab_button)
-    time.sleep(1)
 
-    button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'showLaptop-qJcpoITA')))
-    if button != None:
-        button.click()
-    
-    time.sleep(1)
-    table_element = (By.CLASS_NAME, "table-qJcpoITA")
-    if tab_id == "Economy":
-        table_element = (By.ID, "economy-table")
-        
-    table = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located(table_element)
-    )
+    URL = "https://www.tradingview.com/data-coverage/"
+    driver.get(URL)
 
-    # print('html >> ', table.get_attribute("outerHTML"))
-    table_rows = driver.find_elements(By.CSS_SELECTOR, "table.table-qJcpoITA tbody tr")
-    row_count = len(table_rows)
-    print(f"Number of rows in table after modifying tab {tab_id}: {row_count}")
 
-    exchange_elements = driver.find_elements(By.CLASS_NAME, "rowWrap-qJcpoITA")
+    tradingview_path = "/opt/airflow/FINANCEDATASCRAPER/database/scraping_raw_json/tradingview"
 
-    for element in exchange_elements:
-        exchange_name = ""
-        if(len(element.find_elements(By.XPATH, ".//span[@class='exchangeName-qJcpoITA']"))):
-             exchange_name = element.find_element(By.XPATH, ".//span[@class='exchangeName-qJcpoITA']").text
-             if any(ex["exchangeName"] == exchange_name for ex in exchange_data):
-                continue
-             
-        exchange_desc_name = ""
+    os.makedirs(tradingview_path, exist_ok=True)
 
-        if(len(element.find_elements(By.XPATH, ".//span[@class='exchangeDescName-qJcpoITA']"))):
-            exchange_desc_name = element.find_element(By.XPATH, ".//span[@class='exchangeDescName-qJcpoITA']").text
-            pass
-        
-        country = ""
-        if(len(element.find_elements(By.CLASS_NAME, "cell-qJcpoITA"))):
-            _country_elements = element.find_elements(By.CLASS_NAME, "cell-qJcpoITA")
-            _country_text = _country_elements[0].find_elements(By.TAG_NAME, "span")[-1].text
-            if(_country_text not in ["CURRENCY", "SPOT", "INDICES", "SWAP", "FUTURES", "FUNDAMENTAL"]):
-                country = _country_text
-            pass
-        
-        types = [badge.text for badge in element.find_elements(By.XPATH, ".//span[@class='content-PlSmolIm']")]
-        
-        exchange_data.append({
-            "exchangeName": exchange_name,
-            "exchangeDescName": exchange_desc_name,
-            "country": country,
-            "types": types,
-            "tab": tab_id
-        })
 
-json_output = json.dumps(exchange_data, indent=4, ensure_ascii=False)
-with open(f"{tradingview_path}/exchanges.json", "w", encoding="utf-8") as f:
-    f.write(json_output)
+    tabs = ["Popular", "Stocks& Indices", "Futures", "Forex", "Crypto"]
+    exchange_data = []
 
-driver.quit()
+
+    for tab_id in tabs:
+
+        tab_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, f"//button[@id='{tab_id}']"))
+        )
+        driver.execute_script('arguments[0].click()', tab_button)
+        time.sleep(1)
+
+        button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'showLaptop-qJcpoITA'))
+        )
+        if button:
+            button.click()
+
+        time.sleep(1)
+
+        table_element = (By.CLASS_NAME, "table-qJcpoITA")
+        table = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(table_element)
+        )
+
+        table_rows = driver.find_elements(By.CSS_SELECTOR, "table.table-qJcpoITA tbody tr")
+        row_count = len(table_rows)
+        print(f"Number of rows in table after modifying tab {tab_id}: {row_count}")
+
+        exchange_elements = driver.find_elements(By.CLASS_NAME, "rowWrap-qJcpoITA")
+
+        for element in exchange_elements:
+            exchange_name = ""
+            if len(element.find_elements(By.XPATH, ".//span[@class='exchangeName-qJcpoITA']")):
+                exchange_name = element.find_element(By.XPATH, ".//span[@class='exchangeName-qJcpoITA']").text
+                if any(ex["exchangeName"] == exchange_name for ex in exchange_data):
+                    continue
+
+            exchange_desc_name = ""
+            if len(element.find_elements(By.XPATH, ".//span[@class='exchangeDescName-qJcpoITA']")):
+                exchange_desc_name = element.find_element(By.XPATH, ".//span[@class='exchangeDescName-qJcpoITA']").text
+
+            country = ""
+            if len(element.find_elements(By.CLASS_NAME, "cell-qJcpoITA")):
+                _country_elements = element.find_elements(By.CLASS_NAME, "cell-qJcpoITA")
+                _country_text = _country_elements[0].find_elements(By.TAG_NAME, "span")[-1].text
+                if _country_text not in ["CURRENCY", "SPOT", "INDICES", "SWAP", "FUTURES", "FUNDAMENTAL"]:
+                    country = _country_text
+
+            types = [badge.text for badge in element.find_elements(By.XPATH, ".//span[@class='content-PlSmolIm']")]
+
+            exchange_data.append({
+                "exchangeName": exchange_name,
+                "exchangeDescName": exchange_desc_name,
+                "country": country,
+                "types": types,
+                "tab": tab_id
+            })
+
+    json_output = json.dumps(exchange_data, indent=4, ensure_ascii=False)
+    with open(f"{tradingview_path}/exchanges.json", "w", encoding="utf-8") as f:
+        f.write(json_output)
+
+    driver.quit()
+    return "Exchanges scraped successfully!"
+
+
+if __name__ == "__main__":
+    exchanges_scraper()
